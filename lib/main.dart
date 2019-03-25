f7f5f643bbeeb0cfd178c4cf790b379fe0961594
Shravan Dheep <shravandheep4@gmail.com>
2019-03-25 10:50:53 +0530

    "Add temporary widgets"

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 
-main(){
-	runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget{
+
+	@override
+  	State<StatefulWidget> createState() {
+    	// TODO: implement createState
+    return _MyAppState();
+  	}
+
 }
 
-class MyApp extends StatelessWidget{
+class _MyAppState extends State<MyApp>{
 
-	Widget build(context){
+	List<String> _products = ["Food Tester"];
+
+
+	@override
+	Widget build(BuildContext context){
 		return MaterialApp(
 			home : Scaffold(
 				appBar: AppBar(
 					title:Text('EasyList')
 				),
-			),
+				body : Column(
+					children : [
+						Container(
+							margin: EdgeInsets.all(10.0),
+							child: RaisedButton(
+								onPressed: (){
 
-		);
+								},
+								child : Text("Add Product")
+							),
+						),
+
+						Column(children: _products.map((element) => Card(
+							child :Column(
+								children: <Widget>[
+									Image.asset('assets/food.jpg'),
+									Text(element)
+									],
+								)
+							))
+							.toList(),
+						),
+						 
+						],
+					),
+				),
+			);
 	}
 
 }
