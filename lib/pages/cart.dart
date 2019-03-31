8ba51870f30533796f2c3fbaada7b2862f81dde7
Shravan Dheep <shravandheep4@gmail.com>
2019-04-01 00:32:03 +0530

    Add cart page

---
+++ lib/pages/cart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 5.0,
+        backgroundColor: Colors.green,
+        //centerTitle: true,
+        title : Text("Cart", style: TextStyle(fontSize: 30.0),),
+        centerTitle: true,
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white),onPressed: (){} ),
+        ],
+      ),
+
+      bottomNavigationBar: new Container(
+        color: Colors.white12,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                title: new Text("Total : ", style: TextStyle(fontSize: 15.0, color: Colors.black87),  ),
+                subtitle: new Text("\$100", style: TextStyle(fontSize: 20.0),),
+              ),
+            ),
+
+            Expanded(
+              child: new MaterialButton(
+                onPressed: (){},
+                child: new Text("Check out", style: TextStyle(color: Colors.white),),
+                color: Colors.red,
+                height: 50.0,
+                elevation: 0.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
