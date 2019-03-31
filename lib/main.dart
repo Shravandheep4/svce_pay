16a881d71359174c917af82a2f1443b389a32af0
Shravan Dheep <shravandheep4@gmail.com>
2019-03-31 16:13:17 +0530

    Add homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,120 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main(){
 
-class MyApp extends StatefulWidget{
-
-	@override
-  	State<StatefulWidget> createState() {
-    	// TODO: implement createState
-    return _MyAppState();
-  	}
-
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : HomePage(),
+    )
+  );
 }
 
-class _MyAppState extends State<MyApp>{
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-	List<String> _products = ["Food Tester"];
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.green,
+        //centerTitle: true,
+        title : Text("SVCEpay"),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white),onPressed: (){} ),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white),onPressed: (){} )
+        ],
+      ),
+
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            //header
+
+            new UserAccountsDrawerHeader(
+              accountName: Text('Shrvan Dheep'),
+              accountEmail: Text('shravandheep4@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child : new CircleAvatar(
+                  backgroundColor: Colors.blueGrey,
+                  child: Icon(Icons.person, color: Colors.white),
+                )
+              ),
+              decoration: new BoxDecoration(
+                color : Colors.green
+              ),
+
+            ),
+            
+            //Body
+
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("Home Page"),
+                leading: Icon(Icons.home),
+              ),
+            ),
+
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("My Account"),
+                leading: Icon(Icons.person),
+              ),
+            ),
 
 
-	@override
-	Widget build(BuildContext context){
-		return MaterialApp(
-			home : Scaffold(
-				appBar: AppBar(
-					title:Text('EasyList')
-				),
-				body : Column(
-					children : [
-						Container(
-							margin: EdgeInsets.all(10.0),
-							child: RaisedButton(
-								onPressed: (){
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("My Orders"),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
 
-								},
-								child : Text("Add Product")
-							),
-						),
 
-						Column(children: _products.map((element) => Card(
-							child :Column(
-								children: <Widget>[
-									Image.asset('assets/food.jpg'),
-									Text(element)
-									],
-								)
-							))
-							.toList(),
-						),
-						 
-						],
-					),
-				),
-			);
-	}
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("Stalls"),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
 
+
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("Favourites"),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+
+            new Divider(),
+
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("Settings"),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+
+            new InkWell(
+              onTap: (){},
+              child: ListTile(
+                title : Text("Help"),
+                leading: Icon(Icons.help),
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
 }
