e317b36ece41b5b5a985a8c4ddc10f44da169224
Shravan Dheep <shravandheep4@gmail.com>
2019-04-01 00:29:27 +0530

    Add similar items

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -20,6 +20,21 @@ class _ProductsState extends State<Products> {
       "picture" : "assets/images/products/dress1.jpeg",
       "price": 50,
     },
+
+    {
+      "name": "Some dress",
+      "picture" : "assets/images/products/hills1.jpeg",
+      "price": 50,
+    },
+
+    {
+      "name": "Hills",
+      "picture" : "assets/images/products/hills2.jpeg",
+      "price": 50,
+    },
+
+    
+    
   ];
 
   @override
@@ -55,18 +70,33 @@ class SingleProduct extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Hero(
-        tag: prodName, 
+        tag: new Text("tags"), 
         child: Material(
           child: InkWell(
-            onTap : () => Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new ProductDetails())),
+
+            //onclick method
+            onTap : () => Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new ProductDetails(
+                  productDetailName: prodName,
+                  productDetailPrice: prodPrice,
+                  productDetailPicture: prodPicture,
+                )
+              )
+            ),
 
             child: GridTile(
               child: Image.asset(prodPicture, fit: BoxFit.cover,), 
               footer: Container(
-                color: Colors.white70,
-                child: ListTile(
-                  leading: Text(prodName, style: TextStyle(fontWeight: FontWeight.bold),),
-                  title: Text("\$$prodPrice" ,style: TextStyle(color: Colors.red, fontWeight: FontWeight.w800),),
+                color: Colors.white30,
+                child: new Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(prodName,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16.0)),
+                    ),
+
+                    new Text("\$${prodPrice}", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green, fontSize: 16.0),)
+
+
+                  ],
                 ),
               ),
             ),
